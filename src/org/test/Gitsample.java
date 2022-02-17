61224f2d8f194b03b4944f839afa6cfb8de67212
suryagitacc <suriya881144@gmail.com>
2022-02-17 12:01:58 +0530

    suryagit

---
+++ src/org/test/Gitsample.java
@@ -0,0 +1,28 @@
+package org.test;
+
+public class Gitsample {
+	
+		private int UserId;
+		private String Password;
+		public int getUserId() {
+			return UserId;
+		}
+		public void setUserId(int userId) {
+			UserId = userId;
+		}
+		public String getPassword() {
+			return Password;
+		}
+		public void setPassword(String password) {
+			Password = password;
+		}
+		
+		
+		}
+			
+		
+
+
+
+
+
